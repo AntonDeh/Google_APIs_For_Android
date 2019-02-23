e11c0b221c25deca3e699366f097cc3187c6d3f7
Romanto <Gromachi@gmail.com>
2019-02-23 12:58:46 +0200

    Add files via upload

---
+++ WebScrape/Config.py
@@ -0,0 +1,12 @@
+"""This file contains configurations and const names"""
+
+#Web address
+url = "https://developers.google.com/android/images"
+
+#DB local host
+host_DB = 'mongodb://localhost:27017/'
+
+#DB name
+DB_name = 'GoogleAPIsForAndroid'
+DB_collection = 'FactoryImages'
+
